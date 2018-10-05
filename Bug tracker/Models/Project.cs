b02a5b8148f4af52a08ec69dba4a5df5caa005d9
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-10-05 08:51:44 -0500

    2

---
+++ Bug tracker/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_tracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Project()
+        {
+            Users = new HashSet<ApplicationUser>();
+        }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
