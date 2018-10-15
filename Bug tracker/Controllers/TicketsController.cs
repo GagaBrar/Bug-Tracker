b95f7aa0cab4ce3df3e4206dbb7a2905cb7dc83a
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-10-14 22:27:11 -0500

    Errors fixed

--- Bug tracker/Controllers/TicketsController.cs
+++ Bug tracker/Controllers/TicketsController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity;
 
 namespace Bug_tracker.Controllers
 {
+    [Authorize]
     public class TicketsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
